50ef49abce41a4d39f9336a0cfde9acb38ecf499
Jared Erwin <jaerwin@microsoft.com>
2020-12-07 13:43:48 -0800

    Code review fixes.

--- src/Microsoft.Health.Fhir.Core/Features/Definition/SearchableSearchParameterDefinitionManager.cs
+++ src/Microsoft.Health.Fhir.Core/Features/Definition/SearchableSearchParameterDefinitionManager.cs
@@ -113,7 +113,7 @@ namespace Microsoft.Health.Fhir.Core.Features.Definition
             return _inner.GetSearchParameterHashForResourceType(resourceType);
         }
 
-        public void AddNewSearchParameters(IList<ITypedElement> searchParameters)
+        public void AddNewSearchParameters(IReadOnlyCollection<ITypedElement> searchParameters)
         {
             _inner.AddNewSearchParameters(searchParameters);
         }
